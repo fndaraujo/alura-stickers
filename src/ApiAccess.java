7a33af80a02a4ab2bf7e002f0dc06e8d92492986
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2022-07-19 15:49:15 -0300

    change api url source

--- src/ApiAccess.java
+++ src/ApiAccess.java
@@ -1,5 +1,5 @@
 public class ApiAccess {
-    private static String apiUrl = "https://api.mocki.io/v2/549a5d8b/Top250Movies";
+    private static String apiUrl = "https://alura-filmes.herokuapp.com/conteudos";
     private static String apiKey = "";
 
     public static String getApiAccess() {
