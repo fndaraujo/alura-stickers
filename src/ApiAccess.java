693cd5c971b62f29b12f3f4457111eacc8f8a270
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2022-07-20 02:10:53 -0300

    remove old class ApiAccess

--- src/ApiAccess.java
+++
@@ -1,8 +0,0 @@
-public class ApiAccess {
-    private static String apiUrl = "https://alura-filmes.herokuapp.com/conteudos";
-    private static String apiKey = "";
-
-    public static String getApiAccess() {
-        return apiUrl + apiKey;
-    }
-}
