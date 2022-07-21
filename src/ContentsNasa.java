4ffebd9921a37aaf8c701b02680bb5053dd823bc
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2022-07-21 00:38:40 -0300

    add class contents from nasa

---
+++ src/ContentsNasa.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+
+public class ContentsNasa {
+    public List<Content> getContents(String json) {
+        // TODO: Write code.
+        JsonParser jsonParser = new JsonParser();
+        List<Map<String, String>> apiContents = jsonParser.parse(json);
+        List<Content> contents = new ArrayList<>();
+        for (Map<String, String> attributes : apiContents) {
+            String title = attributes.get("title");
+            String urlImage = attributes.get("url");
+            Content content = new Content(title, urlImage);
+            contents.add(content);
+        }
+        return contents;
+    }
+}
