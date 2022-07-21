aa120b60dccea143afc35490dbc1a330acbf32b1
Fernando Araujo dos Santos <fnd.arj.santos@gmail.com>
2022-07-20 23:08:50 -0300

    add method to get content in api class

--- src/Api.java
+++ src/Api.java
@@ -1,3 +1,10 @@
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.IOException;
@@ -15,6 +22,16 @@ public class Api {
         } catch (IOException ex) {
             ex.printStackTrace();
             return "";
+        }
+    }
+    public static String apiContent() {
+        try {
+            HttpClient httpClient = HttpClient.newHttpClient();
+            HttpRequest httpReq = HttpRequest.newBuilder(URI.create(apiConnection())).GET().build();
+            HttpResponse<String> httpRes = httpClient.send(httpReq, BodyHandlers.ofString());
+            return httpRes.body();
+        } catch (IOException | InterruptedException ex) {
+            throw new RuntimeException(ex);
         }
     }
 }
--- src/App.java
+++ src/App.java
@@ -1,9 +1,4 @@
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 
 import java.util.List;
 import java.util.Map;
@@ -12,20 +7,9 @@ import java.io.InputStream;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        HttpClient httpClient = HttpClient.newHttpClient();
-
-        HttpRequest httpReq = HttpRequest.newBuilder(URI.create(Api.apiConnection()))
-            .GET()
-            .build();
-
-        HttpResponse<String> httpRes = httpClient.send(
-            httpReq,
-            BodyHandlers.ofString()
-        );
-
+        Api api = new Api();
         JsonParser jsonParser = new JsonParser();
-
-        List<Map<String, String>> apiContents = jsonParser.parse(httpRes.body());
+        List<Map<String, String>> apiContents = jsonParser.parse(api.apiContent());
 
         StickerGen sticker = new StickerGen();
         for (Map<String, String> content : apiContents) {
